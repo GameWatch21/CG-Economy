4362752bf3c6034c501d0161e227e8fa37786e85
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2020-04-21 16:18:08 +0700

    Add our Config json to link from the bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const config = require('./config.json')
 
 //This message will be send on your console if the bot are ready to run
 client.once('ready', () => {

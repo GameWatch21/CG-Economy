b76e8e4dee4ace7a9de1e8b261c24e0384a794bf
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2020-04-21 16:14:24 +0700

    Add Message listener

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+//This message will be send on your console if the bot are ready to run
 client.once('ready', () => {
 	console.log('CG-Economy are Ready to Serve');
 });
+//For Listening The Message
+client.on('message', message => {
+	console.log(message.content);
+});
 
+//This is the key Where you need to add your Token
 client.login('Discord Bot Token insert here');

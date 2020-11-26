61f5de12733ce3de0426332c10faa11ea2c0dd88
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2020-11-26 13:41:56 +0700

    Giving comment and adding new command

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const Discord = require('discord.js');
 const { prefix,token } = require('./config.json')
-const client = new Discord.Client();
+const client= new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`)
   client.commands.set(command.name, command);
 }
+
 
 client.once = ('Ready', () => {
   console.log("Ah, it's time for work");
@@ -17,21 +18,42 @@ client.on('message',message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).trim().split(' ');
-  const command = args.shift().toLowerCase();
+  const commandName = args.shift().toLowerCase();
 
+  const command = client.commands.get(commandName)
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+  if (!command) return;
+
+try {
+  command.execute(message, args);
+ } 
+catch (error) {
+  console.error(error);
+  message.reply('there was an error trying to execute that command!');
+}
+
+  // [BASIC COMMANDS]
   if(command === "ping"){
     client.commands.get("ping").execute(message, args);
   }
-  if(command === "info-server"){
-    client.commands.get("info-server").execute(message, args);
-     }
   if(command === "say"){
     message.channel.send(`${args}`)
   }
-  if(command === "prune"){
-    client.commands.get("prune").execute(message, args);
-   
+  if(command === "im"){
+    client.commands.get("im").execute(message, args);
   }
+  // [INFO COMMAND]]
+ if(command === "info-server"){
+    client.commands.get("info-server").execute(message, args);
+ }
+  // [BOT COMMAND]
+  // [MODERATION COMMAND]
+   if(command === "prune"){
+    client.commands.get("prune").execute(message, args);
+}
+  // [ECONOMY COMMAND]
+  // [ADMIN COMMAND]
 });
 
 client.login(token)

c27e1c07142184e2741ca07b75f208be6779f645
GameWatch21 <43901265+GameWatch21@users.noreply.github.com>
2020-11-29 13:11:51 +0700

    create help.js

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'help',
+  description: 'Showing all list of the commands',
+  aliases: ['h'],
+  execute(message, args){
+    const helpEmbed = new Discord.MessageEmbed()
+    .setTitle(`${message.author}, Help Commands Page 1/1`)
+    .setDescription(`Here are the commands:`)
+    .addFields(
+      {name: 'cg!ping' , value: 'Checking if the bot are online or offline' , inline: true},
+      {name: 'cg!info-server' , value: 'Giving the Server Information', inline: true},
+      {name: 'cg!prune' , value: 'Deleting/Pruning messages' , inline: true},
+      {name: 'cg!im' , value: 'Just a weird command for testing' , inline: true},
+      {name: 'cg!reload' , value: 'Reloading a commands' , inline: true},
+      {name: 'cg!reaction' , value: 'Giving a Reaction collector' , inline: true},
+      {name: 'cg!avatar' , value: 'Showing the user avatar link' , inline: true},
+    )
+    .setTimestamp()
+    .setFooter('Created by GameWatch21');
+    
+    message.channel.send(helpEmbed)
+  }
+}
